dfc3c55c4e4fdcac4778d48eb489163427133a00
PeterJeffReyvr <peterjeffreyvr38@gmail.com>
2023-05-02 15:12:23 -0700

    final

---
+++ project/Version.scala
@@ -0,0 +1,9 @@
+object CryptoVersion {
+
+  val scalaCompilers = Seq("2.11.12", "2.12.13", "2.13.6")
+  val spinal        = "1.7.0a"
+
+  private val version = "1.1.1"
+  val tester   = s"$version"
+  val crypto   = s"$version"
+}
